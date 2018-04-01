a3e37dc1b098b43b3cc5cb3d8d6f87f18055df6c
bitsofwinter <bits.of.winter@gmail.com>
2018-04-01 17:47:49 +0200

    Tax report generation given export from cointracking.info

---
+++ report.py
@@ -0,0 +1,51 @@
+import argparse
+import datetime
+import os
+from enum import Enum
+
+from taxdata import PersonalDetails, Fees, Trades, TaxEvent
+import tax
+
+
+class Format(Enum):
+    pdf = 'pdf'
+    sru = 'sru'
+
+    def __str__(self):
+        return self.value
+
+parser = argparse.ArgumentParser(description='Swedish cryptocurrency tax reporting script')
+parser.add_argument('year', type=int,
+                    help='Tax year to create report for')
+parser.add_argument('--trades', help='Read trades from csv file', default='data/trades.csv')
+parser.add_argument('--format', type=Format, choices=list(Format), default=Format.sru,
+                    help='The file format of the generated report')
+parser.add_argument('--decimal-sru', help='Report decimal amounts in sru mode (not supported by Skatteverket yet)', action='store_true')
+parser.add_argument('--exclude-groups', nargs='*', help='Exclude cointracking group from report')
+parser.add_argument('--coin-report', help='Generate report of remaining coins and their cost basis at end of year', action='store_true')
+opts = parser.parse_args()
+
+personal_details = PersonalDetails.read_from("data/personal_details.json")
+trades = Trades.read_from(opts.trades)
+stock_tax_events = TaxEvent.read_stock_tax_events_from("data/stocks.json") if os.path.exists("data/stocks.json") else None
+
+tax_events = tax.compute_tax(trades,
+                             datetime.datetime(year=opts.year,month=1,day=1,hour=0, minute=0),
+                             datetime.datetime(year=opts.year,month=12,day=31,hour=23, minute=59),
+                             exclude_groups=opts.exclude_groups if opts.exclude_groups else [],
+                             coin_report_filename="out/coin_report.csv" if opts.coin_report else None
+                             )
+
+if opts.format == Format.sru and not opts.decimal_sru:
+    tax_events = tax.convert_to_integer_amounts(tax_events)
+
+tax_events = tax.convert_sek_to_integer_amounts(tax_events)
+
+pages = tax.generate_k4_pages(opts.year, personal_details, tax_events, stock_tax_events=stock_tax_events)
+
+if opts.format == Format.sru:
+    tax.generate_k4_sru(pages, personal_details, "out")
+elif opts.format == Format.pdf:
+    tax.generate_k4_pdf(pages, "out")
+
+tax.output_totals(tax_events, stock_tax_events=stock_tax_events)
